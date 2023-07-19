98f58c9a7c17b307be2a8392970f64f7b44da49b
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2023-07-19 15:28:54 +0300

    Use the MutableStateFlow.update { ... } method to update the state that view model is holding

--- app/src/main/java/com/mixlr/panos/uilayerdemo/ScoreViewModel.kt
+++ app/src/main/java/com/mixlr/panos/uilayerdemo/ScoreViewModel.kt
@@ -4,16 +4,21 @@ import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class ScoreViewModel : ViewModel() {
     fun homeGoal() {
         val (home, guest) = _uiState.value.score.split("-")
-        _uiState.value = ScoreUiState("${home.toInt() + 1}-$guest")
+        _uiState.update {
+            it.copy(score = "${home.toInt() + 1}-$guest")
+        }
     }
 
     fun guestGoal() {
         val (home, guest) = _uiState.value.score.split("-")
-        _uiState.value = ScoreUiState("$home-${guest.toInt() + 1}")
+        _uiState.update {
+            it.copy(score = "$home-${guest.toInt() + 1}")
+        }
     }
 
     private val _uiState = MutableStateFlow(ScoreUiState("0-0"))
